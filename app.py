773ee3692b380697ecbac2cff711f5aa5a36f3ce
Max Zaytsev <maxim.zaytsev.1305@gmail.com>
2021-03-04 16:54:16 +0300

    Added Projects page
    Updated favicon
    Removed footer
    Updated *.html files

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def home():
     return flask.render_template('main/home.html')
 
 
+@app.route('/projects')
+def projects():
+    return flask.render_template('main/projects.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
