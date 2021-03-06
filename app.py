f71233750fd4d255ba3ee16d8ba31247032a248b
Max Zaytsev <maxim.zaytsev.1305@gmail.com>
2021-03-06 13:30:58 +0300

    Added about page & error handler
    Refactoring few files
    Updated few files

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import flask
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return flask.render_template('main/home.html')
 
@@ -17,6 +19,16 @@ def projects():
 @app.route('/links')
 def links():
     return flask.render_template('main/links.html')
+
+
+@app.route('/about')
+def about():
+    return flask.render_template('main/about.html')
+
+
+@app.errorhandler(HTTPException)
+def error_handler(e):
+    return flask.render_template('main/error.html', error_code=e.code), e.code
 
 
 if __name__ == "__main__":
