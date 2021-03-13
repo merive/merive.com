384520a0137dbde5070355d42cfb1a8b1fffcc53
* <maxim.zaytsev.1305@gmail.com>
2021-03-13 09:23:02 +0300

    Added docstring for functions and classes

--- app.py
+++ app.py
@@ -17,31 +17,39 @@ db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/home')
 def home():
+    """Home page"""
     return flask.render_template('main/home.html')
 
 
 @app.route('/projects')
 def projects():
+    """Projects page""" 
     return flask.render_template('main/projects.html')
 
 
 @app.route('/links')
 def links():
+    """Links page""" 
     return flask.render_template('main/links.html')
 
 
 @app.route('/about')
 def about():
+    """About page""" 
     return flask.render_template('main/about.html')
 
 
 @app.errorhandler(HTTPException)
 def error_handler(e):
+    """Return Error page""" 
     return flask.render_template('main/error.html', error_code=e.code), e.code
 
 
 # Press1MTimes code
 class P1MTBase(db.Model):
+
+    """P1MT DataBase for files"""
+
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(25), unique=False, nullable=False)
     version_code = db.Column(db.String(6), unique=False, nullable=False)
@@ -52,42 +60,51 @@ class P1MTBase(db.Model):
 
     @staticmethod
     def add_in_db(filename, version_code, data):
+        """Adds new file in database"""
         new = P1MTBase(file_name=filename, version_code=version_code, data=data)
         db.session.add(new)
         db.session.commit()
 
     @staticmethod
-    def remove_rows():
+    def remove_elms():
+        """Remove all elements of database""" 
         P1MTBase.query.delete()
 
 
 @app.route('/P1MT')
 def press1mtimes():
+    """P1MT Page""" 
     data = P1MTBase.query.filter_by().first()
     return flask.render_template('P1MT/home.html', version=data.version_code)
 
 
 @app.route('/press1mtimes/download')
 def download_p1mt():
+    """Download P1MT page"""
     data = P1MTBase.query.filter_by().first()
     return send_file(BytesIO(data.data), attachment_filename=data.file_name, as_attachment=True)
 
 
 @app.route('/P1MT/update')
 def update_p1mt():
+    """P1MT file update page"""
     return flask.render_template('P1MT/update.html')
 
 
 @app.route('/P1MT/upload', methods=['POST'])
 def upload_p1mt():
+    """Updates P1MT file in database"""
     check_hash(request.form['key'])
-    P1MTBase().remove_rows()
+    P1MTBase().remove_elms()
     P1MTBase().add_in_db(request.files['file'].filename, request.form['version_code'], request.files['file'].read())
     return flask.render_template('P1MT/update.html', result="File has been uploaded successfully.")
 
 
 # MTools code
 class MToolsBase(db.Model):
+
+    """MTools database"""
+
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(25), unique=False, nullable=False)
     version_code = db.Column(db.String(6), unique=False, nullable=False)
@@ -99,6 +116,7 @@ class MToolsBase(db.Model):
 
     @staticmethod
     def add_in_db(filename, version_code, data):
+        """Updates MTools files in database"""
         new = MToolsBase(file_name=filename, version_code=version_code, data=data,
                          file_type=filename[len(filename) - 3:].upper())
         db.session.add(new)
@@ -106,6 +124,7 @@ class MToolsBase(db.Model):
 
     @staticmethod
     def remove_rows(filename):
+        """Removes old element in database"""
         try:
             MToolsBase.query.filter_by(file_type=filename[len(filename) - 3:].upper()).first().delete()
         except AttributeError:
@@ -114,28 +133,33 @@ class MToolsBase(db.Model):
 
 @app.route('/MTools')
 def mtools():
+    """MTools page"""
     return flask.render_template('MTools/home.html')
 
 
 @app.route("/MTools/download")
 def download_mtools():
+    """Download MTools page"""
     data = MToolsBase.query.filter_by().first()
     return flask.render_template('MTools/download.html', version=data.version_code)
 
 
 @app.route('/MTools/download/<file_type>')
 def save_mtools(file_type: str):
+    """Saves MTools files on your PC"""
     data = MToolsBase.query.filter_by(file_type=file_type).first()
     return send_file(BytesIO(data.data), attachment_filename=data.file_name, as_attachment=True)
 
 
 @app.route('/MTools/update')
 def update_mtools():
+    """Update MTools page"""
     return flask.render_template('MTools/update.html')
 
 
 @app.route('/MTools/upload', methods=['POST'])
 def upload_mtools():
+    """Uploads new MTools file in database"""
     check_hash(request.form['key'])
     MToolsBase().remove_rows(request.files['file'].filename)
     MToolsBase().add_in_db(request.files['file'].filename, request.form['version_code'], request.files['file'].read())
@@ -145,10 +169,12 @@ def upload_mtools():
 # Parzibot code
 @app.route('/Parzibot')
 def parzibot():
+    """Parzibot page"""
     return flask.render_template('Parzibot/home.html', link=os.environ.get('ParzibotLink'))
 
 
 def check_hash(u_key):
+    """Checks password for uploading files"""
     if hashlib.sha224(bytes(u_key, encoding='utf-8')).hexdigest() != os.environ.get('KEY'):
         return flask.render_template('main/error.html'), 403
 
